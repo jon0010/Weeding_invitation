794c9cc6a98e0e1255a732a356c5dc934fc8665e
Jon Nahuel Pereyra <jonnahuel78@gmail.com>
2023-07-21 23:03:44 -0300

    fix/ sobre responsive

--- src/sobre/Sobre.jsx
+++ src/sobre/Sobre.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
+import imagenSobre from "../assets/imagenSobre.jpeg";
 import "./sobre.css";
 import CssBaseline from "@mui/material/CssBaseline";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Hidden from "@mui/material/Hidden";
 
 const defaultTheme = createTheme();
 
@@ -16,41 +16,18 @@ const Sobre = () => {
     <ThemeProvider theme={defaultTheme}>
       <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage:
-              "url(https://scontent.feze9-1.fna.fbcdn.net/v/t39.30808-6/352133999_6275139392541758_8980223177570125181_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeF319KWIBd7FtkBs3R-sFGgWqfsflh9TDlap-x-WH1MOaN-QBWo28JmXz6LkA6511tzz7Gx58IyyMCT3tm3CL2h&_nc_ohc=f0M3QMNT0ZwAX_eR4UV&_nc_oc=AQn0ILLoEcCBoCo9UlPVlv2xXHoJWzj0IBgvdXZoaYc2Mkf_C8oJmMQzInvJSR0NKrc7N6hqIwRRZlj3tz_PYarv&_nc_ht=scontent.feze9-1.fna&oh=00_AfBMZlE8yIL5FB_hiCbcUCc7RqTxVqm9S7pYi8_MrC1wQQ&oe=649307C4)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-        <Grid
-          item
-          xs={12}
-          sm={8}
-          md={5}
-          component={Paper}
-          elevation={6}
-          square
-          sx={{ width: "100%" }}
-        >
-          <Box
+        {/* Texto y Logo - md y superiores */}
+        <Hidden smDown>
+          <Grid
+            item
+            xs={12}
+            md={6}
             sx={{
-              my: 12,
-              mx: 0,
               display: "flex",
+              justifyContent: "space-around", // Centrar horizontalmente
+              alignItems: "center", // Centrar verticalmente
               flexDirection: "column",
-              alignItems: "center",
-              width: "100%",
+              height: "100%", // Ocupar la altura completa
             }}
           >
             <Typography component="h1" variant="h5">
@@ -59,8 +36,71 @@ const Sobre = () => {
             <Link to="/invitacion">
               <img className="logo" src={logo} alt="logo" />
             </Link>
-          </Box>
-        </Grid>
+          </Grid>
+        </Hidden>
+        {/* Imagen de fondo - md y superiores */}
+        <Hidden smDown>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{
+              backgroundImage: `url(${imagenSobre})`,
+              backgroundRepeat: "no-repeat",
+              backgroundColor: (t) =>
+                t.palette.mode === "light"
+                  ? t.palette.grey[50]
+                  : t.palette.grey[900],
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              height: "100%", // Ocupar la altura completa
+            }}
+          />
+        </Hidden>
+        {/* Logo - pantallas sm */}
+        <Hidden mdUp>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              display: "flex",
+              justifyContent: "center", // Centrar horizontalmente
+              alignItems: "center", // Centrar verticalmente
+              flexDirection: "column",
+              height: "66.66%", // Ocupar 2/3 de la altura disponible
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Toca aquí
+            </Typography>
+            <Link to="/invitacion">
+              <img
+                className="logo"
+                src={logo}
+                alt="logo"
+                style={{ width: "50%", marginTop: "20px" }}
+              />
+            </Link>
+          </Grid>
+        </Hidden>
+        {/* Imagen de fondo - pantallas sm */}
+        <Hidden mdUp>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              backgroundImage: `url(${imagenSobre})`,
+              backgroundRepeat: "no-repeat",
+              backgroundColor: (t) =>
+                t.palette.mode === "light"
+                  ? t.palette.grey[50]
+                  : t.palette.grey[900],
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              height: "33.33%", // Ocupar 1/3 de la altura disponible
+            }}
+          />
+        </Hidden>
       </Grid>
     </ThemeProvider>
   );
